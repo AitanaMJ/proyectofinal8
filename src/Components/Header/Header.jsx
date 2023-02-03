7d1ace39fbde1f84782bc2a7f815c473f1d43e18
milagros <milyromano.8@gmail.com>
2023-02-02 22:49:50 -0300

    Se creo el Header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+
+import '../Styles/header.css'
+function Header() {
+    return (
+        <>
+            <header>
+                Rotaract Club - Juan Bautista Alberdi
+            </header>
+        </>
+    )
+}
+
+export default Header;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,10 +2,13 @@
 import Navbar from "../Components/Navbar/Navbar";
 import HomeBody from "../Components/HomeBody/HomeBody";
 import Footer from "../Components/Footer/Footer";
+import Header from "../Components/Header/Header";
+
 
 const HomePage = () => {
     return (
         <>
+            <Header/>
             <Navbar/>
             <HomeBody/>
             <Footer/>
